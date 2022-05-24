0c93099afdc025228f4bf6f98a963c586821fa06
Vladyslav <baskov228322@gmail.com>
2022-05-24 20:06:29 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles/main.scss'
+import photo from './assets/photo.jpg'
+console.log("vlad asd");
+
+
+const photoImg = document.querySelector('img');
+photoImg.src = photo
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    mode: "development",
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist")
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.jpg$/,
+                type: 'asset/resource'
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "webpack app",
+            filename: "index.html",
+            template: 'src/index.html'
+        }),
+        // new BundleAnalyzerPlugin(),
+    ]
+}
